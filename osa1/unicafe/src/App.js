dd4ddb91543cdcb69fe0eaae925343b5ecea22da
erregung <kingelin@protonmail.com>
2021-06-30 22:28:28 +0300

    do part1/b

---
+++ osa1/unicafe/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Give feedback</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
