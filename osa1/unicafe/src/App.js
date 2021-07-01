ec49ddd370da02f5840344a622689ed553fcf94e
erregung <kingelin@protonmail.com>
2021-07-01 19:49:46 +0300

    do 1.7

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,10 +1,44 @@
 import React, { useState } from 'react'
 
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>No feedback given yet.</div>
+    )
+  }
+  return (
+    <div>
+        <h1>Statistics</h1>
+        <p>Good: {props.good}</p>
+        <p>Neutral: {props.neutral}</p>
+        <p>Bad: {props.bad}</p>
+        <p>{props.allClicks.join(' ')}</p>
+        <p>Total: {props.good+props.bad+props.neutral}</p>
+        <p>Average: {(props.good+(props.bad*(-1))) / (props.good+props.bad+props.neutral)}</p>
+        <p>Positive: {(props.good / (props.good+props.bad+props.neutral))*100} %</p>
+    </div>
+  )
+}
+
 const App = () => {
-  // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const goodClick = () => {
+    setGood(good+1)
+    setAll(allClicks.concat('g'))
+  }
+
+  const neutralClick = () => {
+    setNeutral(neutral+1)
+    setAll(allClicks.concat('n'))
+  }
+  const badClick = () => {
+    setBad(bad+1)
+    setAll(allClicks.concat('b'))
+  }
 
   return (
     <div>
@@ -12,25 +46,22 @@ const App = () => {
         <h2>Tehtävät 1.6. - 1.14.</h2>
         <h1>Give feedback</h1>
 
-        <button onClick={() => setGood(good+1)}>
-          Good
-        </button>
+        <button onClick={goodClick}>Good</button>
 
-        <button onClick={() => setNeutral(neutral+1)}>
-          Neutral
-        </button>
+        <button onClick={neutralClick}>Neutral</button>
 
-        <button onClick={() => setBad(bad+1)}>
-          Bad
-        </button>
+        <button onClick={badClick}>Bad</button>
         
-        <h1>Statistics</h1>
-        <p>Good: {good}</p>
-        <p>Neutral: {neutral}</p>
-        <p>Bad: {bad}</p>
+        <History allClicks={allClicks} />
+
       </div>
     </div>
   )
 }
 
 export default App
+
+//1.8 refaktoroi tilastojen näyttäminen omaan komponenttiin Statistics
+//1.9 jos palautteita ei ole, ei näytetä tilastoja
+//1.10 eriytä Button ja StatisticLine omiksi komponenteikseen
+//1.11 näytä tilastit html-taulukkona
