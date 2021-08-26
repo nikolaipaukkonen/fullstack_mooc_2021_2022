74162ec3ac6beba4d1fbd6ae71d8723723db52e5
erregung <kingelin@protonmail.com>
2021-08-26 13:49:32 +0300

    do phonebook 2.6-2.10

--- osa2/phonebook/src/App.js
+++ osa2/phonebook/src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Person from './components/Person'
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
 
-function App() {
+const App = () => {
+  const [ persons, setPersons] = useState([
+    { name:'asd', number:'123' },
+    { name:'asdf', number:'1234' }
+  ])
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  const [ newFilter, setNewFilter] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+
+    if (newName.length <= 0) {
+      alert('Name is required!')
+      return;
+    }
+
+    if (newNumber.length <= 0) {
+      alert('Number is required!')
+      return;
+    }
+    const personObject = {
+      name : newName,
+      number : newNumber
+    }
+
+    if (persons.find(person => person.name === personObject.name)) {
+      alert(`${personObject.name} already exists!`)
+      return;
+    }
+    
+    setPersons(persons.concat(personObject))
+    setNewName('')
+    setNewNumber('')
+  }
+
+  const handleNameChange = (event) => { 
+    setNewName(event.target.value)
+  }
+  const handleNumberChange = (event) => { 
+    setNewNumber(event.target.value)
+  }
+  const handleFilterChange = (event) => { 
+    setNewFilter(event.target.value)
+  }
+
+  const numbersToShow = persons.filter(person => {
+    if (newFilter.length <= 0) {
+      return true
+    }
+    
+    return person.name.includes(newFilter)
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <h2>Phonebook</h2>
+      <Filter newFilter={newFilter} handleFilterChange={handleFilterChange} />
+
+      <h3>Add a new contact</h3>
+      <PersonForm addPerson={addPerson} newName={newName} handleNameChange={handleNameChange}
+      newNumber={newNumber} handleNumberChange={handleNumberChange} />
+
+      <h3>Numbers</h3>
+      {numbersToShow.map(person => 
+            <Person key={person.name} name={person.name} number={person.number} />
+        )}
+
     </div>
-  );
+  )
+
 }
 
-export default App;
+export default App
